de7e672a7edd2d0525dbb42640d65a3441eb9f0a
konsti219 <37149441+konsti219@users.noreply.github.com>
2021-11-23 22:59:59 +0100

    upakcli: Implement file processing logic

--- upakcli/src/main.rs
+++ upakcli/src/main.rs
@@ -1,4 +1,12 @@
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::path::Path;
+use std::process::exit;
+use std::time::SystemTime;
+
 use clap::{Parser, Subcommand};
+
+use upak;
 
 /// Command line tool for working with Unreal Engine .pak files
 #[derive(Parser, Debug)]
@@ -38,5 +46,44 @@ enum Commands {
 fn main() {
     let args = Args::parse();
 
-    println!("{:?}", args);
+    let start = SystemTime::now();
+
+    match args.commands {
+        Commands::CheckHeader { pakfile } => {
+            let file = open_file(Path::new(&pakfile));
+            let pak = upak::PakFile::new(&file);
+            check_header(pak);
+        },
+        _ => {
+            eprintln!("Not implemented yet");
+        }
+    }
+
+
+    println!(
+        "upakcli took {:?} seconds...",
+        start.elapsed().unwrap().as_secs_f32()
+    )
+}
+
+fn open_file(path: &Path) -> File {
+    match OpenOptions::new().read(true).open(&path) {
+        Ok(file) => file,
+        Err(_) => {
+            eprintln!("Could not find/open file");
+            exit(1);
+        }
+    }
+}
+
+fn check_header(mut pak_file: upak::PakFile) -> upak::PakFile {
+    match pak_file.load_records() {
+        Ok(_) => println!("Header is ok"),
+        Err(e) => {
+            eprintln!("Error reading header: {:?}", e);
+            exit(1);
+        }
+    }
+    println!("Found {:?} records", pak_file.records.len());
+    return pak_file;
 }
