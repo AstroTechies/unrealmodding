7d760513df1c91a978542ec8e25338bd648bd365
konsti219 <37149441+konsti219@users.noreply.github.com>
2021-11-24 17:46:52 +0100

    upak: Implement header parsing

--- upak/src/lib.rs
+++ upak/src/lib.rs
@@ -27,9 +27,9 @@ header:
 
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{BufReader, Error, ErrorKind, Seek, SeekFrom};
+use std::io::{BufReader, Error, ErrorKind, Read, Seek, SeekFrom};
 
-use byteorder::{BigEndian, ReadBytesExt};
+use byteorder::{BigEndian, LittleEndian, ReadBytesExt};
 
 use num_enum::{IntoPrimitive, TryFromPrimitive};
 
@@ -95,6 +95,93 @@ impl<'file> PakFile<'file> {
         // read and check magic bytes
         if self.reader.read_u32::<BigEndian>()? != UE4_PAK_MAGIC {
             return Err(Error::new(ErrorKind::Other, "File is not a valid pak file"));
+        }
+
+        // read and check file version
+        let file_version = self.reader.read_u32::<LittleEndian>()?;
+        if file_version != 8 {
+            return Err(Error::new(
+                ErrorKind::Other,
+                format!("Unsupported file version {}", file_version),
+            ));
+        }
+        self.file_version = file_version;
+
+        if self.file_version == 8 {
+            // read index offset
+            let offset = self.reader.read_u64::<LittleEndian>()?;
+            self.reader.seek(SeekFrom::Start(offset))?;
+
+            // read mount point
+            let mount_point_len = self.reader.read_u32::<LittleEndian>()?;
+            let mut mount_point_buf = vec![0u8; mount_point_len as usize];
+            self.reader.read_exact(&mut mount_point_buf)?;
+            self.mount_point = mount_point_buf;
+
+            // read record count
+            let record_count = self.reader.read_u32::<LittleEndian>()?;
+
+            // read records data
+            for _i in 0..record_count {
+                // read record name
+                let record_name_len = self.reader.read_u32::<LittleEndian>()?;
+                let mut record_name_buf = vec![0u8; record_name_len as usize];
+                self.reader.read_exact(&mut record_name_buf)?;
+                let mut record_name = match String::from_utf8(record_name_buf) {
+                    Ok(record_name) => record_name,
+                    Err(err) => {
+                        return Err(Error::new(
+                            ErrorKind::Other,
+                            format!("Record name is not valid utf8: {:?}", err),
+                        ));
+                    }
+                };
+                record_name.pop();
+
+                // read record offset
+                let record_offset = self.reader.read_u64::<LittleEndian>()?;
+                // read record size
+                let record_size = self.reader.read_u64::<LittleEndian>()?;
+                // read record decompressed size
+                let record_decompressed_size = self.reader.read_u64::<LittleEndian>()?;
+                // read record compression method
+                let record_compression_method =
+                    CompressionMethod::try_from_primitive(self.reader.read_u32::<LittleEndian>()?).unwrap();
+
+                // seek over hash
+                self.reader.seek_relative(20)?;
+
+                if record_compression_method != CompressionMethod::None {
+                    // read block count
+                    let block_count = self.reader.read_u32::<LittleEndian>()?;
+
+                    // seek over block data
+                    self.reader
+                        .seek(SeekFrom::Current(16 * block_count as i64))?;
+                }
+
+                // skip is_encrypted and block size
+                self.reader.seek(SeekFrom::Current(5))?;
+
+                // add record
+                self.records.insert(
+                    record_name,
+                    PakRecord {
+                        file_version: self.file_version,
+                        offset: record_offset,
+                        size: record_size,
+                        decompressed_size: record_decompressed_size,
+                        compression_method: record_compression_method,
+                        compression_blocks: Vec::new(),
+                        hash: Vec::new(),
+                    },
+                );
+            }
+        } else {
+            return Err(Error::new(
+                ErrorKind::Other,
+                format!("Unsupported file version {}", self.file_version),
+            ));
         }
 
         Ok(())
