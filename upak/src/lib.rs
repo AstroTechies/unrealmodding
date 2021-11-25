7eeeb27c6f6d4a4627e6809fe68061ed862e71a3
konsti219 <37149441+konsti219@users.noreply.github.com>
2021-11-25 14:34:20 +0100

    upak: Implement record reading

--- upak/src/lib.rs
+++ upak/src/lib.rs
@@ -30,6 +30,7 @@ use std::fs::File;
 use std::io::{BufReader, Error, ErrorKind, Read, Seek, SeekFrom};
 
 use byteorder::{BigEndian, LittleEndian, ReadBytesExt};
+use flate2::{read::ZlibDecoder};
 
 use num_enum::{IntoPrimitive, TryFromPrimitive};
 
@@ -146,7 +147,8 @@ impl<'file> PakFile<'file> {
                 let record_decompressed_size = self.reader.read_u64::<LittleEndian>()?;
                 // read record compression method
                 let record_compression_method =
-                    CompressionMethod::try_from_primitive(self.reader.read_u32::<LittleEndian>()?).unwrap();
+                    CompressionMethod::try_from_primitive(self.reader.read_u32::<LittleEndian>()?)
+                        .unwrap();
 
                 // seek over hash
                 self.reader.seek_relative(20)?;
@@ -185,5 +187,124 @@ impl<'file> PakFile<'file> {
         }
 
         Ok(())
+    }
+
+    pub fn read_record(&mut self, record_name: &String) -> Result<Vec<u8>, Error> {
+        // find record
+        let record = match self.records.get(record_name) {
+            Some(record) => record,
+            None => {
+                return Err(Error::new(
+                    ErrorKind::Other,
+                    format!("Record {} not found", record_name),
+                ));
+            }
+        };
+
+        if self.file_version == 8 {
+            if record.compression_method == CompressionMethod::None {
+                // seek to data
+                self.reader.seek(SeekFrom::Start(record.offset + 0x35))?;
+
+                let mut buf = vec![0u8; record.decompressed_size as usize];
+                self.reader.read_exact(&mut buf)?;
+
+                return Ok(buf);
+            } else if record.compression_method == CompressionMethod::Zlib {
+                // skip unimportant data
+                self.reader.seek(SeekFrom::Start(record.offset + 0x30))?;
+
+                // read blocks
+                let mut blocks = Vec::new();
+
+                // read block count
+                let block_count = self.reader.read_u32::<LittleEndian>()?;
+
+                // read blocks
+                for _i in 0..block_count {
+                    // read block start
+                    let block_start = self.reader.read_u64::<LittleEndian>()?;
+                    // read block end
+                    let block_end = self.reader.read_u64::<LittleEndian>()?;
+
+                    // add block
+                    blocks.push(Block {
+                        start: block_start,
+                        size: block_end - block_start,
+                    });
+                }
+
+                // is_encrypted byte
+                let mut buf1 = [0u8; 1];
+                self.reader.read_exact(&mut buf1)?;
+                let is_encrypted = buf1[0] != 0;
+                // if is_encrypted return error
+                if is_encrypted {
+                    return Err(Error::new(
+                        ErrorKind::Other,
+                        format!(
+                            "Record {} is encrypted, enrcryption is not supported.",
+                            record_name
+                        ),
+                    ));
+                }
+
+                // read block size
+                let block_size = self.reader.read_u32::<LittleEndian>()?;
+
+                // calculate header size and sub from blocks
+                let header_size = self.reader.stream_position()? - record.offset;
+                blocks = blocks
+                    .iter()
+                    .map(|block| Block {
+                        start: block.start - header_size,
+                        size: block.size,
+                    })
+                    .collect();
+
+                // allocate buffers
+                let mut record_data = vec![0u8; record.decompressed_size as usize];
+                let mut compressed_data = vec![0u8; record.size as usize];
+
+                // read all record compressed data (we are already at the start of the first block)
+                self.reader.read_exact(&mut compressed_data)?;
+
+                for i in 0..blocks.len() {
+                    let block = &blocks[i];
+
+                    let block_compressed_data =
+                        &compressed_data[block.start as usize..(block.start + block.size) as usize];
+
+                    // decompress block
+                    let mut decoder = ZlibDecoder::new(&block_compressed_data[..]);
+
+                    // read decompressed data
+                    let decompressed_start = block_size as usize * i;
+                    let mut decompressed_end = block_size as usize * (i + 1);
+                    if decompressed_end > record.decompressed_size as usize {
+                        decompressed_end = record.decompressed_size as usize;
+                    }
+
+                    decoder.read_exact(&mut record_data[decompressed_start..decompressed_end])?;
+                }
+
+                return Ok(record_data);
+            } else {
+                // we should never get here
+                return Err(Error::new(
+                    ErrorKind::Other,
+                    format!(
+                        "Unsupported compression method {:?}",
+                        record.compression_method
+                    ),
+                ));
+            }
+        } else {
+            // we should never get here
+            return Err(Error::new(
+                ErrorKind::Other,
+                format!("Unsupported file version {}", self.file_version),
+            ));
+        }
     }
 }
