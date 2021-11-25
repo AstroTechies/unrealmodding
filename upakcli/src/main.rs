f5a53b3beeb7e5837d99c72f5ff58ae2781cab13
konsti219 <37149441+konsti219@users.noreply.github.com>
2021-11-25 20:12:45 +0100

    upakcli: Implement checking of all records

--- upakcli/src/main.rs
+++ upakcli/src/main.rs
@@ -23,23 +23,23 @@ enum Commands {
     /// Adds files to myapp
     Check {
         /// The .pak file to check
-        pakfile: String
+        pakfile: String,
     },
     CheckHeader {
         /// The .pak file to check
-        pakfile: String
+        pakfile: String,
     },
     Extract {
         /// The .pak file to extract
         pakfile: String,
         /// The directory to extract to
-        outdir: Option<String>
+        outdir: Option<String>,
     },
     Create {
         /// The .pak file to create
         pakfile: String,
         /// The directory to create the file from
-        indir: String
+        indir: String,
     },
 }
 
@@ -51,14 +51,36 @@ fn main() {
     match args.commands {
         Commands::CheckHeader { pakfile } => {
             let file = open_file(Path::new(&pakfile));
-            let pak = upak::PakFile::new(&file);
-            check_header(pak);
-        },
+            let mut pak = upak::PakFile::new(&file);
+            check_header(&mut pak);
+        }
+        Commands::Check { pakfile } => {
+            let file = open_file(Path::new(&pakfile));
+            let mut pak = upak::PakFile::new(&file);
+            check_header(&mut pak);
+
+            // TODO: get rid of this clone
+            for (i, (record_name, _)) in pak.records.clone().iter().enumerate() {
+                println!("Record {}: {}", i.to_string(), record_name);
+
+                match pak.read_record(&record_name) {
+                    Ok(_) => (),
+                    Err(e) => {
+                        eprintln!(
+                            "Error reading record {}: {}, Error: {}",
+                            i.to_string(),
+                            record_name,
+                            e
+                        );
+                        exit(1);
+                    }
+                }
+            }
+        }
         _ => {
             eprintln!("Not implemented yet");
         }
     }
-
 
     println!(
         "upakcli took {:?} seconds...",
@@ -76,7 +98,7 @@ fn open_file(path: &Path) -> File {
     }
 }
 
-fn check_header(mut pak_file: upak::PakFile) -> upak::PakFile {
+fn check_header(pak_file: &mut upak::PakFile) {
     match pak_file.load_records() {
         Ok(_) => println!("Header is ok"),
         Err(e) => {
@@ -85,5 +107,4 @@ fn check_header(mut pak_file: upak::PakFile) -> upak::PakFile {
         }
     }
     println!("Found {:?} records", pak_file.records.len());
-    return pak_file;
 }
