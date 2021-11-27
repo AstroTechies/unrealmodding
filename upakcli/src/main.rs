af1b11523c574b6950769a6b4e1055f22ec4d674
konsti219 <37149441+konsti219@users.noreply.github.com>
2021-11-27 18:16:29 +0100

    upakcli: Implement extraction of paks

--- upakcli/src/main.rs
+++ upakcli/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::fs::OpenOptions;
 use std::path::Path;
+use std::io::Write;
 use std::process::exit;
 use std::time::SystemTime;
 
@@ -72,6 +73,87 @@ fn main() {
                             record_name,
                             e
                         );
+                        exit(1);
+                    }
+                }
+            }
+        }
+        Commands::Extract { pakfile, outdir } => {
+            let path = Path::new(&pakfile);
+            let file = open_file(&path);
+            let mut pak = upak::PakFile::new(&file);
+            check_header(&mut pak);
+
+            // temp values required to extend lifetimes outside of match scope
+            let temp;
+            let temp2;
+            let output_folder: &Path = match outdir {
+                Some(ref outdir) => {
+                    temp = outdir.clone();
+                    Path::new(&temp)},
+                None => {
+                    temp2 = path.parent().unwrap().join(path.file_stem().unwrap());
+                    &temp2
+                },
+            };
+
+            println!("Extracting to {}", output_folder.display());
+
+            // TODO: get rid of this clone
+            for (i, (record_name, _)) in pak.records.clone().iter().enumerate() {
+                match pak.read_record(&record_name) {
+                    Ok(record_data) => {
+                        let path = Path::new(output_folder).join(&record_name);
+                        let dir_path = match path.parent() {
+                            Some(dir) => dir,
+                            None => {
+                                eprintln!(
+                                    "No parent directories found! {}: {}",
+                                    i.to_string(),
+                                    record_name
+                                );
+                                exit(1);
+                            }
+                        };
+
+                        // Create the parent directories, then files.
+                        match std::fs::create_dir_all(dir_path) {
+                            Ok(_) => {
+                                // Create the file
+                                let mut file = match File::create(&path) {
+                                    Ok(file) => file,
+                                    Err(_) => {
+                                        eprintln!(
+                                            "Error creating file! {}: {:?}",
+                                            i.to_string(),
+                                            path
+                                        );
+                                        exit(1);
+                                    }
+                                };
+                                // Write the file
+                                match file.write_all(&record_data) {
+                                    Ok(_) => {
+                                        println!("Record {}: {}", i.to_string(), record_name);
+                                    }
+                                    Err(_) => {
+                                        eprintln!(
+                                            "Error writing to file! {}: {:?}",
+                                            i.to_string(),
+                                            path
+                                        );
+                                        exit(1);
+                                    }
+                                }
+                            }
+                            Err(_) => {
+                                eprintln!("Error creating directories! {:?}", dir_path);
+                                exit(1);
+                            }
+                        };
+                    }
+                    Err(_) => {
+                        eprintln!("Error reading record {}: {}", i.to_string(), record_name);
                         exit(1);
                     }
                 }
