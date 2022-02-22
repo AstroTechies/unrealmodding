5018a0f5100b95c81180ef83df9ba13b2f982202
localcc <localcmail@gmail.com>
2022-02-22 17:09:06 +0200

    uasset: color properties implemented

---
+++ uasset/src/uasset/properties/color_property.rs
@@ -0,0 +1,44 @@
+use std::io::{Cursor, Error, ErrorKind, Read};
+
+use byteorder::{LittleEndian, ReadBytesExt};
+
+use crate::{uasset::{unreal_types::Guid, cursor_ext::CursorExt, types::Color}, optional_guid};
+
+#[derive(Debug)]
+pub struct ColorProperty {
+    property_guid: Option<Guid>,
+    color: Color<u8>
+}
+
+#[derive(Debug)]
+pub struct LinearColorProperty {
+    property_guid: Option<Guid>,
+    color: Color<f32>
+}
+
+impl ColorProperty {
+    pub fn new(cursor: &mut Cursor<Vec<u8>>, include_header: bool) -> Result<Self, Error> {
+        let property_guid = optional_guid!(cursor, include_header);
+        let color = Color::from_argb(cursor.read_i32::<LittleEndian>()?);
+        Ok(ColorProperty {
+            property_guid,
+            color
+        })
+    }
+}
+
+impl LinearColorProperty {
+    pub fn new(cursor: &mut Cursor<Vec<u8>>, include_header: bool) -> Result<Self, Error> {
+        let property_guid = optional_guid!(cursor, include_header);
+        let color = Color::new(
+            cursor.read_f32::<LittleEndian>()?,
+            cursor.read_f32::<LittleEndian>()?,
+            cursor.read_f32::<LittleEndian>()?,
+            cursor.read_f32::<LittleEndian>()?
+        );
+        Ok(LinearColorProperty {
+            property_guid,
+            color
+        })
+    }
+}
--- uasset/src/uasset/types.rs
+++ uasset/src/uasset/types.rs
@@ -11,3 +11,23 @@ impl<T> Vector<T> {
         Vector{ x, y, z }
     }
 }
+
+#[derive(Debug)]
+pub struct Color<T> {
+    r: T,
+    g: T,
+    b: T,
+    a: T
+}
+
+impl<T> Color<T> {
+    pub fn new(r: T, g: T, b: T, a: T) -> Self {
+        Color { r, g, b, a }
+    }
+}
+
+impl Color<u8> {
+    pub fn from_argb(argb: i32) -> Self {
+        Color::new((argb >> 24) & 0xff, (argb >> 16) & 0xff, (argb >> 8) & 0xff, argb & 0xff)
+    }
+}
