0b38d85f9a2954c66167933c9d5295a0ba27d1ec
localcc <localcmail@gmail.com>
2022-03-14 12:55:30 +0200

    uasset: Per platform float property implemented

--- uasset/src/uasset/properties/per_platform_property.rs
+++ uasset/src/uasset/properties/per_platform_property.rs
@@ -53,3 +53,28 @@ impl PerPlatformIntProperty {
         })
     }
 }
+
+pub struct PerPlatformFloatProperty {
+    name: FName,
+    property_guid: Guid,
+    value: Vec<f32>
+}
+
+impl PerPlatformFloatProperty {
+    pub fn new(name: FName, cursor: &mut Cursor<Vec<u8>>, include_header: bool, length: i64) -> Result<Self, Error> {
+        let property_guid = optional_guid!(cursor, include_header);
+
+        let num_entries = cursor.read_i32::<LittleEndian>()?;
+        let value = Vec::with_capacity(num_entries);
+
+        for i in 0..num_entries {
+            value[i] = cursor.read_f32()?;
+        }
+
+        Ok(PerPlatformFloatProperty {
+            name,
+            property_guid,
+            value
+        })
+    }
+}
