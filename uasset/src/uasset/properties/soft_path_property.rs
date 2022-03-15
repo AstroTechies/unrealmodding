9cd4a826d242103e33e66135bde5dfb38c63370c
localcc <localcmail@gmail.com>
2022-03-15 10:46:48 +0100

    uasset: Soft path property implemented

--- uasset/src/uasset/properties/soft_path_property.rs
+++ uasset/src/uasset/properties/soft_path_property.rs
@@ -2,19 +2,37 @@ use std::io::{Cursor, Error, ErrorKind};
 
 use byteorder::{LittleEndian, ReadBytesExt};
 
-use crate::{uasset::{unreal_types::{Guid, FName}, cursor_ext::CursorExt, Asset}, optional_guid};
+use crate::{uasset::{unreal_types::{Guid, FName}, cursor_ext::CursorExt, Asset, ue4version::VER_UE4_ADDED_SOFT_OBJECT_PATH}, optional_guid};
 
-pub struct SoftObjectPathProperty {
+pub struct SoftPathProperty {
     name: FName,
     property_guid: Option<Guid>,
-    asset_path_name: FName,
-    sub_path: String,
-    path: String
+    asset_path_name: Option<FName>,
+    sub_path: Option<String>,
+    path: Option<String>
 }
 
-impl SoftObjectPathProperty {
+impl SoftPathProperty {
     pub fn new(name: FName, cursor: &mut Cursor<Vec<u8>>, include_header: bool, length: i64, asset: &Asset) -> Result<Self, Error> {
         let property_guid = optional_guid!(cursor, include_header);
         
-    }        
+        let mut path = None;
+        let mut asset_path_name = None;
+        let mut sub_path = None;
+
+        if asset.engine_version < VER_UE4_ADDED_SOFT_OBJECT_PATH {
+            path = Some(cursor.read_string()?);
+        } else {
+            asset_path_name = Some(asset.read_fname()?);
+            sub_path = Some(cursor.read_string()?);
+        }
+
+        Ok(SoftPathProperty {
+            name,
+            property_guid,
+            asset_path_name,
+            sub_path,
+            path
+        })
+    }
 }
