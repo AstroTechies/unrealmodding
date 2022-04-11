12fe28e2500baf6ebabce1c81eb2f9379f05c94b
konsti219 <37149441+konsti219@users.noreply.github.com>
2022-04-12 00:22:35 +0200

    astro_modloader: Add basic config for Astro

--- astro_modloader/src/main.rs
+++ astro_modloader/src/main.rs
@@ -1,6 +1,29 @@
 use unreal_modloader;
 
+mod astro_integrator;
+
+struct AstroGameConfig;
+
+impl unreal_modloader::config::GameConfig<astro_integrator::AstroIntegratorConfig>
+    for AstroGameConfig
+{
+    fn get_integrator_config(&self) -> &astro_integrator::AstroIntegratorConfig {
+        &astro_integrator::AstroIntegratorConfig
+    }
+
+    fn get_game_name(&self) -> String {
+        "Astro".to_string()
+    }
+
+    fn get_window_title(&self) -> String {
+        "Astroneer Modloader".to_string()
+    }
+}
+
 fn main() {
     println!("Astroneer Modloader");
-    unreal_modloader::run();
+
+    let config = AstroGameConfig;
+
+    unreal_modloader::run(&config);
 }
