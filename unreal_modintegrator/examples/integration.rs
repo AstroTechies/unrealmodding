864890da012983c91c4bb2561b859e1011646ad4
localcc <localcmail@gmail.com>
2022-04-12 17:53:43 +0200

    unreal_modintegrator: examples added

---
+++ unreal_modintegrator/examples/integration.rs
@@ -0,0 +1,77 @@
+use std::{collections::HashMap, io, env};
+
+use unreal_asset::ue4version::VER_UE4_23;
+use unreal_modintegrator::IntegratorConfig;
+use unreal_pak::PakFile;
+
+pub struct Config;
+
+fn handle_persistent_actors(
+    _data: &(),
+    _integrated_pak: &mut PakFile,
+    _game_paks: &mut Vec<PakFile>,
+    actors: Vec<&serde_json::Value>,
+) -> Result<(), io::Error> {
+    println!("{:?}", actors);
+    Ok(())
+}
+
+impl<'data> IntegratorConfig<'data, (), io::Error> for Config {
+    fn get_data(&self) -> &'data () {
+        &()
+    }
+
+    fn get_handlers(
+        &self,
+    ) -> std::collections::HashMap<
+        String,
+        Box<
+            dyn FnMut(
+                &(),
+                &mut unreal_pak::PakFile,
+                &mut Vec<unreal_pak::PakFile>,
+                Vec<&serde_json::Value>,
+            ) -> Result<(), io::Error>,
+        >,
+    > {
+        let mut handlers: std::collections::HashMap<
+            String,
+            Box<
+                dyn FnMut(
+                    &(),
+                    &mut unreal_pak::PakFile,
+                    &mut Vec<unreal_pak::PakFile>,
+                    Vec<&serde_json::Value>,
+                ) -> Result<(), io::Error>,
+            >,
+        > = HashMap::new();
+
+        handlers.insert(String::from("persistent_actors"), Box::new(handle_persistent_actors));
+
+        handlers
+    }
+
+    fn get_integrator_version(&self) -> String {
+        String::from("0.1.0")
+    }
+
+    fn get_refuse_mismatched_connections(&self) -> bool {
+        true
+    }
+
+    fn get_engine_version(&self) -> i32 {
+        VER_UE4_23
+    }
+}
+
+fn main() {
+    let config = Config;
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.len() != 2 {
+        println!("Invalid args. Expected {{game_path}} {{mods_path}}");
+    }
+
+    let game_path = args[0].clone();
+    let mods_path = args[1].clone();
+    unreal_modintegrator::integrate_mods(&config, &mods_path, &game_path).unwrap();
+}
