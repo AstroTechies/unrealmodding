3c3e9772437efe40900bdd075b63629a746880a3
konsti219 <37149441+konsti219@users.noreply.github.com>
2022-04-12 20:13:08 +0200

    unreal_modintegrator: fix use of unstable crate

--- unreal_modintegrator/src/assets.rs
+++ unreal_modintegrator/src/assets.rs
@@ -1,13 +1,11 @@
-use include_flate::flate;
-
 // todo: use bulk data
 // todo: make assets universal
-flate!(pub(crate) static INTEGRATOR_API_ASSET: [u8] from "assets/IntegratorAPI.uasset");
-flate!(pub(crate) static INTEGRATOR_STATICS_ASSET: [u8] from "assets/IntegratorStatics.uasset");
-flate!(pub(crate) static INTEGRATOR_STATICS_BP_ASSET: [u8] from "assets/IntegratorStatics_BP.uasset");
-flate!(pub(crate) static LIST_OF_MODS_ASSET: [u8] from "assets/ListOfMods.uasset");
-flate!(pub(crate) static METADATA_JSON: [u8] from "assets/metadata.json");
-flate!(pub(crate) static MOD_ASSET: [u8] from "assets/Mod.uasset");
-flate!(pub(crate) static MOD_MISMATCH_WIDGET_ASSET: [u8] from "assets/ModMismatchWidget.uasset");
-flate!(pub(crate) static SERVER_MOD_COMPONENT_ASSET: [u8] from "assets/ServerModComponent.uasset");
-flate!(pub(crate) static SYNC_MODE_ASSET: [u8] from "assets/SyncMode.uasset");
+pub(crate) const INTEGRATOR_API_ASSET: &'static[u8] = include_bytes!("../assets/IntegratorAPI.uasset");
+pub(crate) const INTEGRATOR_STATICS_ASSET: &'static[u8] = include_bytes!("../assets/IntegratorStatics.uasset");
+pub(crate) const INTEGRATOR_STATICS_BP_ASSET: &'static[u8] = include_bytes!("../assets/IntegratorStatics_BP.uasset");
+pub(crate) const LIST_OF_MODS_ASSET: &'static[u8] = include_bytes!("../assets/ListOfMods.uasset");
+pub(crate) const METADATA_JSON: &'static[u8] = include_bytes!("../assets/metadata.json");
+pub(crate) const MOD_ASSET: &'static[u8] = include_bytes!("../assets/Mod.uasset");
+pub(crate) const MOD_MISMATCH_WIDGET_ASSET: &'static[u8] = include_bytes!("../assets/ModMismatchWidget.uasset");
+pub(crate) const SERVER_MOD_COMPONENT_ASSET: &'static[u8] = include_bytes!("../assets/ServerModComponent.uasset");
+pub(crate) const SYNC_MODE_ASSET: &'static[u8] = include_bytes!("../assets/SyncMode.uasset");
