588a03e751e834b27610b401822d5dd4d112d4f7
konsti219 <37149441+konsti219@users.noreply.github.com>
2022-04-24 20:03:43 +0200

    astro_modloader: Provide function for getting GameBuild

--- astro_modloader/src/main.rs
+++ astro_modloader/src/main.rs
@@ -1,7 +1,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::path::PathBuf;
+
 use unreal_modintegrator::IntegratorConfig;
 use unreal_modloader::config::GameConfig;
+use unreal_modloader::version::GameBuild;
 
 mod astro_integrator;
 use astro_integrator::AstroIntegratorConfig;
@@ -26,6 +29,19 @@ where
 
     fn get_window_title(&self) -> String {
         "Astroneer Modloader".to_string()
+    }
+
+    fn get_game_build(&self, install_path: &PathBuf) -> Option<GameBuild> {
+        let version_file_path = install_path.join("build.version");
+        if !version_file_path.is_file() {
+            info!("{:?} not found", version_file_path);
+            return None;
+        }
+
+        let version_file = std::fs::read_to_string(&version_file_path).unwrap();
+        let game_build_string = version_file.split(' ').next().unwrap().to_owned();
+
+        GameBuild::try_from(&game_build_string).ok()
     }
 }
 
