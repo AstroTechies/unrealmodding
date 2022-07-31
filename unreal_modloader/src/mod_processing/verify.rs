d31709fc8be25d94bf97cdd2073fe85b531593b1
localcc <localcmail@gmail.com>
2022-07-31 18:32:17 +0200

    unreal_modloader: allow . inside modid

--- unreal_modloader/src/mod_processing/verify.rs
+++ unreal_modloader/src/mod_processing/verify.rs
@@ -13,7 +13,7 @@ use regex::Regex;
 pub fn verify_mod_file_name(mod_id: &str) -> bool {
     lazy_static! {
         //                                   000  -ModName69   -1  .1  .1  _P.pak
-        static ref RE: Regex = Regex::new(r"^\d{3}-[a-zA-Z0-9]+-\d+.\d+.\d+_P.pak$").unwrap();
+        static ref RE: Regex = Regex::new(r"^\d{3}-[a-zA-Z0-9\.]+-\d+.\d+.\d+_P.pak$").unwrap();
     }
 
     RE.is_match(mod_id)
