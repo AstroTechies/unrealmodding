d54441eae998fae96ed06c83b9cf8be880c2e790
localcc <work@localcc.cc>
2022-12-18 21:41:38 +0100

    unreal_asset: add CDO modification unit test

---
+++ unreal_asset/tests/cdo_modification.rs
@@ -0,0 +1,61 @@
+use unreal_asset::{
+    cast,
+    engine_version::EngineVersion,
+    error::Error,
+    exports::{normal_export::NormalExport, Export, ExportBaseTrait},
+    flags::EObjectFlags,
+    properties::{object_property::ObjectProperty, PropertyDataTrait},
+    unreal_types::{FName, PackageIndex},
+    Asset,
+};
+
+mod shared;
+
+macro_rules! test_asset {
+    () => {
+        concat!(
+            env!("CARGO_MANIFEST_DIR"),
+            "/tests/assets/general/Astroneer_prebulk/Augment_BroadBrush"
+        )
+    };
+}
+
+const TEST_ASSET: &[u8] = include_bytes!(concat!(test_asset!(), ".uasset"));
+
+#[test]
+fn cdo_modification() -> Result<(), Error> {
+    let mut asset = Asset::new(TEST_ASSET.to_vec(), None);
+    asset.set_engine_version(EngineVersion::VER_UE4_23);
+
+    asset.parse_data()?;
+    shared::verify_reparse(&mut asset)?;
+
+    let cdo_export: &mut NormalExport = asset
+        .exports
+        .iter_mut()
+        .find(|e| {
+            e.get_base_export().object_flags & EObjectFlags::RF_CLASS_DEFAULT_OBJECT.bits()
+                == EObjectFlags::RF_CLASS_DEFAULT_OBJECT.bits()
+        })
+        .map(|e| cast!(Export, NormalExport, e))
+        .flatten()
+        .expect("Failed to find cdo export");
+
+    let pickup_actor = cdo_export
+        .properties
+        .iter_mut()
+        .find(|e| e.get_name().content == "PickupActor")
+        .expect("Failed to find PickupActor");
+
+    *pickup_actor = ObjectProperty {
+        name: FName::from_slice("PickupActor"),
+        property_guid: None,
+        duplication_index: 0,
+        value: PackageIndex::new(0),
+    }
+    .into();
+
+    shared::verify_reparse(&mut asset)?;
+
+    Ok(())
+}
