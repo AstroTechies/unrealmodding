f5dc508b4a76c5dcdad887e211b12e6fc9bad6f7
localcc <work@localcc.cc>
2023-02-02 13:44:13 +0100

    unreal_modintegrator: asset downloading through github_helpers

---
+++ github_helpers/src/lib.rs
@@ -0,0 +1,56 @@
+use std::{error::Error, fs::File, io};
+
+use reqwest::{
+    blocking,
+    header::{self, HeaderMap},
+};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct Release {
+    pub assets: Vec<ReleaseAsset>,
+}
+#[derive(Debug, Deserialize)]
+pub struct ReleaseAsset {
+    pub name: String,
+    pub browser_download_url: String,
+}
+
+impl ReleaseAsset {
+    pub fn download(&self, file: &mut File) -> Result<(), Box<dyn Error>> {
+        let mut response = blocking::Client::new()
+            .get(&self.browser_download_url)
+            .headers(get_headers())
+            .send()?;
+
+        io::copy(&mut response, file)?;
+
+        Ok(())
+    }
+}
+
+fn get_headers() -> HeaderMap {
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        header::USER_AGENT,
+        "reqwest/unrealmodding-github_helpers"
+            .parse()
+            .expect("Invalid user agent"),
+    );
+
+    headers
+}
+
+pub fn get_latest_release(repo_url: &str) -> Result<Release, Box<dyn Error>> {
+    let headers = get_headers();
+    let api_response = blocking::Client::new()
+        .get(format!(
+            "https://api.github.com/repos/{repo_url}/releases/latest"
+        ))
+        .headers(headers.clone())
+        .send()?;
+
+    let release = api_response.json()?;
+
+    Ok(release)
+}
