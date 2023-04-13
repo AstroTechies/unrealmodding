f069cc80c47a0c87e0a9d8bd7bedd931cc4536d4
localcc <work@localcc.cc>
2023-04-13 14:58:06 +0200

    unreal_asset: move to thiserror for error declaration.

--- unreal_asset/src/error.rs
+++ unreal_asset/src/error.rs
@@ -1,118 +1,118 @@
 //! All errors thrown by unreal_asset
 
-use std::error;
-use std::fmt::{Display, Formatter};
 use std::io;
 use std::string::{FromUtf16Error, FromUtf8Error};
 
 use num_enum::{TryFromPrimitive, TryFromPrimitiveError};
+use thiserror::Error;
+use unreal_helpers::error::FStringError;
 
 use crate::custom_version::FAssetRegistryVersionType;
 
 /// Thrown when kismet bytecode failed to deserialize
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum KismetError {
+    /// Kismet token was invalid
+    #[error("{0}")]
     InvalidToken(Box<str>),
+    /// Unknown kismet expression
+    #[error("{0}")]
     UnknownExpression(Box<str>),
 }
 
 impl KismetError {
+    /// Create a `KismetError` for an invalid token
     pub fn token(msg: String) -> Self {
         KismetError::InvalidToken(msg.into_boxed_str()) // todo: maybe not allocate a string
     }
 
+    /// Create a `KismetError` for an unknown expression
     pub fn expression(msg: String) -> Self {
         KismetError::UnknownExpression(msg.into_boxed_str())
     }
 }
 
-impl Display for KismetError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match *self {
-            KismetError::InvalidToken(ref err) => f.write_str(err),
-            KismetError::UnknownExpression(ref err) => f.write_str(err),
-        }
-    }
-}
-
 /// Thrown when a usmap file failed to deserialize
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum UsmapError {
+    /// Unsupported usmap compression
+    #[error("Unsupported compression: {0}")]
     UnsupportedCompression(u8),
+    /// Invalid compressiondata
+    #[error("Invalid compression data")]
     InvalidCompressionData,
 }
 
 impl UsmapError {
+    /// Create an `UsmapError` for an unsupported compresion
     pub fn unsupported_compression(compression: u8) -> Self {
         UsmapError::UnsupportedCompression(compression)
     }
 
+    /// Create an `UsmapError` for invalid compression data
     pub fn invalid_compression_data() -> Self {
         UsmapError::InvalidCompressionData
     }
 }
 
-impl Display for UsmapError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match *self {
-            UsmapError::UnsupportedCompression(compression) => {
-                write!(f, "Unsupported compression: {compression}")
-            }
-            UsmapError::InvalidCompressionData => write!(f, "Invalid compression data"),
-        }
-    }
-}
-
 /// Thrown when asset registry failed to deserialize
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum RegistryError {
+    /// Invalid registry index
+    #[error("Invalid index: {0}")]
     InvalidIndex(i32),
+    /// Invalid registry value for a given version
+    #[error("Invalid value {0} for asset registry with version {1}")]
     Version(Box<str>, FAssetRegistryVersionType),
+    /// Other
+    #[error("{0}")]
     Other(Box<str>),
 }
 
 impl RegistryError {
+    /// Create a `RegistryError` for an invalid registry index
     pub fn index(index: i32) -> Self {
         RegistryError::InvalidIndex(index)
     }
 
+    /// Create a `RegistryError` for an invalid value for a given version
     pub fn version(msg: String, version: FAssetRegistryVersionType) -> Self {
         RegistryError::Version(msg.into_boxed_str(), version)
     }
 
+    /// Create an other `RegistryError`
     pub fn other(msg: String) -> Self {
         RegistryError::Other(msg.into_boxed_str())
     }
 }
 
-impl Display for RegistryError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match *self {
-            RegistryError::InvalidIndex(index) => write!(f, "Invalid index: {index}"),
-            RegistryError::Version(ref msg, version) => write!(
-                f,
-                "Invalid value {msg} for asset registry with version {version}"
-            ),
-            RegistryError::Other(ref err) => f.write_str(err),
-        }
-    }
-}
-
 /// Thrown when a property failed to deserialize
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum PropertyError {
+    /// Header data was expected when serializing, but none was found
+    #[error("include_header: true on headerless property")]
     HeaderlessProperty,
+    /// A field was None when Some(...) was expected
+    #[error("{0} is None, expected {1}")]
     PropertyFieldNone(Box<str>, Box<str>),
+    /// A `StructProperty` is invalid
+    #[error("{0}")]
     InvalidStruct(Box<str>),
+    /// An `ArrayProperty` is invalid
+    #[error("{0}")]
     InvalidArrayType(Box<str>),
+    /// Other
+    #[error("{0}")]
     Other(Box<str>),
 }
 
 impl PropertyError {
+    /// Create a `PropertyError` for a property where a header was expected when serializing, but none was found
     pub fn headerless() -> Self {
         PropertyError::HeaderlessProperty
     }
 
+    /// Create a `PropertyError` for a field that was expected to have a value, but was None
     pub fn property_field_none(field_name: &str, expected: &str) -> Self {
         PropertyError::PropertyFieldNone(
             field_name.to_string().into_boxed_str(),
@@ -120,190 +120,93 @@ impl PropertyError {
         )
     }
 
+    /// Create a `PropertyError` for an invalid `StructProperty`
     pub fn invalid_struct(msg: String) -> Self {
         PropertyError::InvalidStruct(msg.into_boxed_str())
     }
 
+    /// Create a `PropertyError` for an invalid `ArrayProperty`
     pub fn invalid_array(msg: String) -> Self {
         PropertyError::InvalidArrayType(msg.into_boxed_str())
     }
 
+    /// Create an other `PropertyError`
     pub fn other(msg: String) -> Self {
         PropertyError::Other(msg.into_boxed_str())
     }
 }
 
-impl Display for PropertyError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match *self {
-            PropertyError::HeaderlessProperty => {
-                write!(f, "include_header: true on headerless property")
-            }
-            PropertyError::PropertyFieldNone(ref field_name, ref expected) => {
-                write!(f, "{field_name} is None expected {expected}")
-            }
-            PropertyError::InvalidStruct(ref err) => f.write_str(err),
-            PropertyError::InvalidArrayType(ref err) => f.write_str(err),
-            PropertyError::Other(ref err) => f.write_str(err),
-        }
-    }
-}
-
-#[derive(Debug)]
-pub enum ErrorCode {
-    Io(io::Error),
-    FString(unreal_helpers::error::FStringError),
-    Utf8(FromUtf8Error),
-    Utf16(FromUtf16Error),
+/// Error type
+#[derive(Error, Debug)]
+pub enum Error {
+    /// An `std::io::Error` occured
+    #[error(transparent)]
+    Io(#[from] io::Error),
+    /// An `FStringError` occured
+    #[error(transparent)]
+    FString(#[from] FStringError),
+    /// A `FromUtf8Error` occured
+    #[error(transparent)]
+    Utf8(#[from] FromUtf8Error),
+    /// A `FromUtf16Error` occured
+    #[error(transparent)]
+    Utf16(#[from] FromUtf16Error),
+    /// Expected data was not found
+    #[error("{0}")]
     NoData(Box<str>),
+    /// An FName pointer was out of range
+    #[error("Cannot read FName, index: {0}, name map size: {1}")]
     FName(i32, usize),
+    /// The file is invalid
+    #[error("{0}")]
     InvalidFile(Box<str>),
+    /// A package index is invalid
+    #[error("{0}")]
     InvalidPackageIndex(Box<str>),
+    /// An enum value is invalid
+    #[error("{0}")]
     InvalidEnumValue(Box<str>),
+    /// Part of the library is not implemented
+    #[error("{0}")]
     Unimplemented(Box<str>),
-    Kismet(KismetError),
-    Property(PropertyError),
-    Registry(RegistryError),
-    Usmap(UsmapError),
-}
-
-#[derive(Debug)]
-pub struct Error {
-    code: ErrorCode,
+    /// A `KismetError` occured
+    #[error(transparent)]
+    Kismet(#[from] KismetError),
+    /// A `PropertyError` occcured
+    #[error(transparent)]
+    Property(#[from] PropertyError),
+    /// A `RegistryError` occured
+    #[error(transparent)]
+    Registry(#[from] RegistryError),
+    /// A `UsmapError` occured
+    #[error(transparent)]
+    Usmap(#[from] UsmapError),
 }
 
 impl Error {
     pub fn no_data(msg: String) -> Self {
-        Error {
-            code: ErrorCode::NoData(msg.into_boxed_str()),
-        }
+        Error::NoData(msg.into_boxed_str())
     }
 
     pub fn fname(index: i32, name_map_size: usize) -> Self {
-        Error {
-            code: ErrorCode::FName(index, name_map_size),
-        }
+        Error::FName(index, name_map_size)
     }
 
     pub fn invalid_file(msg: String) -> Self {
-        Error {
-            code: ErrorCode::InvalidFile(msg.into_boxed_str()),
-        }
+        Error::InvalidFile(msg.into_boxed_str())
     }
 
     pub fn invalid_package_index(msg: String) -> Self {
-        Error {
-            code: ErrorCode::InvalidPackageIndex(msg.into_boxed_str()),
-        }
+        Error::InvalidPackageIndex(msg.into_boxed_str())
     }
 
     pub fn unimplemented(msg: String) -> Self {
-        Error {
-            code: ErrorCode::Unimplemented(msg.into_boxed_str()),
-        }
-    }
-}
-
-impl From<io::Error> for Error {
-    fn from(e: io::Error) -> Self {
-        Error {
-            code: ErrorCode::Io(e),
-        }
-    }
-}
-
-impl From<unreal_helpers::error::FStringError> for Error {
-    fn from(e: unreal_helpers::error::FStringError) -> Self {
-        Error {
-            code: ErrorCode::FString(e),
-        }
-    }
-}
-
-impl From<FromUtf8Error> for Error {
-    fn from(e: FromUtf8Error) -> Self {
-        Error {
-            code: ErrorCode::Utf8(e),
-        }
-    }
-}
-
-impl From<FromUtf16Error> for Error {
-    fn from(e: FromUtf16Error) -> Self {
-        Error {
-            code: ErrorCode::Utf16(e),
-        }
+        Error::Unimplemented(msg.into_boxed_str())
     }
 }
 
 impl<T: TryFromPrimitive> From<TryFromPrimitiveError<T>> for Error {
     fn from(e: TryFromPrimitiveError<T>) -> Self {
-        Error {
-            code: ErrorCode::InvalidEnumValue(e.to_string().into_boxed_str()),
-        }
-    }
-}
-
-impl From<KismetError> for Error {
-    fn from(e: KismetError) -> Self {
-        Error {
-            code: ErrorCode::Kismet(e),
-        }
-    }
-}
-
-impl From<PropertyError> for Error {
-    fn from(e: PropertyError) -> Self {
-        Error {
-            code: ErrorCode::Property(e),
-        }
-    }
-}
-
-impl From<RegistryError> for Error {
-    fn from(e: RegistryError) -> Self {
-        Error {
-            code: ErrorCode::Registry(e),
-        }
-    }
-}
-
-impl From<UsmapError> for Error {
-    fn from(e: UsmapError) -> Self {
-        Error {
-            code: ErrorCode::Usmap(e),
-        }
-    }
-}
-
-impl Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        Display::fmt(&self.code, f)
-    }
-}
-
-impl error::Error for Error {}
-
-impl Display for ErrorCode {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match *self {
-            ErrorCode::Io(ref err) => Display::fmt(err, f),
-            ErrorCode::FString(ref err) => Display::fmt(err, f),
-            ErrorCode::Utf8(ref err) => Display::fmt(err, f),
-            ErrorCode::Utf16(ref err) => Display::fmt(err, f),
-            ErrorCode::NoData(ref err) => Display::fmt(err, f),
-            ErrorCode::InvalidFile(ref err) => f.write_str(err),
-            ErrorCode::InvalidPackageIndex(ref err) => f.write_str(err),
-            ErrorCode::InvalidEnumValue(ref err) => f.write_str(err),
-            ErrorCode::Unimplemented(ref err) => f.write_str(err),
-            ErrorCode::Kismet(ref err) => Display::fmt(err, f),
-            ErrorCode::Property(ref err) => Display::fmt(err, f),
-            ErrorCode::Registry(ref err) => Display::fmt(err, f),
-            ErrorCode::Usmap(ref err) => Display::fmt(err, f),
-            ErrorCode::FName(index, name_map_size) => write!(
-                f,
-                "Cannot read FName, index: {index}, name map size: {name_map_size}"
-            ),
-        }
+        Error::InvalidEnumValue(e.to_string().into_boxed_str())
     }
 }
